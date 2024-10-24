ab5232b4a9f52db22914615d10d85fac329a3515
wengtim <wengtim05@proton.me>
2024-10-24 23:18:19 +0800

    added path for mason

--- config.fish
+++ config.fish
@@ -28,7 +28,7 @@ set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /path/to/python3.12/bin $PATH
 set -gx PATH /Users/wengtim/Library/Python/3.12/bin $PATH
 set -gx PATH /Users/wengtim/go/bin $PATH
-set -gx PATH $PATH ~/.local/share/nvim/mason/bin
+set -gx PATH ~/.local/share/nvim/mason/bin $PATH
 
 # Starship prompt
 starship init fish | source
