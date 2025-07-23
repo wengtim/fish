825208810688c3453bb896a74233ee14c6c2f4cb
wengtim <wengtim05@proton.me>
2025-07-23 15:35:01 +0800

    update: added pycharm cli

--- config.fish
+++ config.fish
@@ -17,6 +17,7 @@ alias mg='mongosh "mongodb+srv://pythonproject.f8iut.mongodb.net/" --apiVersion 
 alias n="node"
 alias p3="python3"
 alias pip="pip3"
+alias pycharm="charm"
 alias sofish="source ~/.config/fish/config.fish"
 alias sql="sqlite3"
 alias v="nvim"
